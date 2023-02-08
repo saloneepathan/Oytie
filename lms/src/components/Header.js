e97692b33b381e1650c482ddd7343bf9dbffeb02
Hardik <hardik.kotangale@gmail.com>
2023-02-08 20:51:50 +0530

    modified:   src/components/Header.js

--- lms/src/components/Header.js
+++ lms/src/components/Header.js
@@ -11,13 +11,13 @@ function Header(props) {
         <Navbar.Brand href="#logo" className='Logo'> <img src={props.logo} width="232" height="72" alt='none'  className="d-inline-block align-top"/></Navbar.Brand> 
         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
         <Navbar.Collapse id="responsive-navbar-nav" >
-          <Nav className="me-auto fs-4.5">
-            <Nav.Link className="hover-underline-animation me-auto" href="#home">{props.home}</Nav.Link>
-            <Nav.Link className="hover-underline-animation me-auto" href="#lmsPortal">{props.navLink1}</Nav.Link>
-            <Nav.Link className="hover-underline-animation me-auto" href="#jobPortal">{props.navLink2}</Nav.Link>
+          <Nav className="me-auto fs-4.5 ">
+            <Nav.Link className="hover-underline-animation me-auto text-light" href="#home">{props.home}</Nav.Link>
+            <Nav.Link className="hover-underline-animation me-auto text-light" href="#lmsPortal">{props.navLink1}</Nav.Link>
+            <Nav.Link className="hover-underline-animation me-auto text-light" href="#jobPortal">{props.navLink2}</Nav.Link>
           </Nav>
           <Nav>
-            <Nav.Link  className="hover-underline-animation me-auto  fs-4.5" href="#contactUs">{props.contactUs} </Nav.Link>
+            <Nav.Link  className="hover-underline-animation me-auto  fs-4.5 text-light" href="#contactUs">{props.contactUs} </Nav.Link>
             
           </Nav>
         </Navbar.Collapse>
